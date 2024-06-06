5c57b62ca548a414b14f44761fb152d42e5f8f9f
profAlpay <alpayalbayrak59@gmail.com>
2024-06-07 00:20:10 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -14,8 +14,7 @@ class Analysis():
         subscriptions_df = pd.read_csv(self.subscriptions_file, header=None, names=['user_id', 'subscription_name', 'some_other_value'])
         prices_df = pd.read_csv(self.prices_file)
         
-        # Debug: Print the columns of prices_df to check the column names
-        # print("Columns in prices_df:", prices_df.columns)
+        
         
         # Ensure column names are correct
         prices_df.columns = [col.strip() for col in prices_df.columns]  # Remove any leading/trailing spaces
@@ -30,10 +29,6 @@ class Analysis():
                 price = prices_df[prices_df['subscription_name'] == subscription]['price'].values[0]
                 total_cost += price
 
-        print(f"{user_id} toplam abonelik ücreti: ₺{total_cost:.2f}")
+        print(f"{user_id} total subscription fee: ₺{total_cost:.2f}")
 
-# Create an instance of the Analysis class
-#analysis = Analysis('subscriptions.csv', 'prices.csv')
 
-# Calculate and print the total price for a specific user
-#analysis.total_price('user1')
